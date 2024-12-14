dc3cbe151c51356afef99aee3207400dd34fd809
paritoshpalai07 <148952813+paritoshpalai07@users.noreply.github.com>
2024-12-14 23:29:30 +0530

    Update settings.py

--- learning_logs/settings.py
+++ learning_logs/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%mjiaxri4^oh^t=w5o@#xx&x0)23@sp(79)^jgx9_5njry(di5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
