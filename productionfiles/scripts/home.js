7d6fd5d75d9b66fb227e03c2ad205a6d44a7378e
Paritosh Palai <148952813+paritoshpalai07@users.noreply.github.com>
2025-09-10 12:18:42 +0530

    Delete productionfiles directory

--- productionfiles/scripts/home.js
+++
@@ -1,9 +0,0 @@
-let inputForm = document.getElementById('input-form');
-
-    function togglehide(){
-        console.log('clicked');
-        inputForm.classList.toggle('hidden');
-        // document.querySelector("#topics-list div").classList.toggle('hidden');
-
-    }
-
